867461ce970afe8dae69a0dc70ae7e659f024515
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2023-08-10 15:19:25 -0700

    Added a monster class
    Made the original monster class that all other classes will build on.

---
+++ script.js
@@ -0,0 +1,10 @@
+class Monster {
+    constructor(name, location, rating, weakness, strength, type) {
+      this.name = name;
+      this.location = location;
+      this.rating = rating;
+      this.weakness = weakness;
+      this.strength = strength;
+      this.type = type;
+    }
+  }
